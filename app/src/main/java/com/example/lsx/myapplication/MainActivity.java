7b9ed2c0a9d34a2bee6d56746fe0ad8645ecf109
omen810 <8561073@qq.com>
2016-07-21 16:04:01 +0800

    done

--- app/src/main/java/com/example/lsx/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lsx/myapplication/MainActivity.java
@@ -2,35 +2,43 @@ package com.example.lsx.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button btn_submit;
+    private Button btn_submit,btn_add;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*btn_submit = (Button) findViewById(R.id.button_submit);
-        btn_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "you clicked submit", Toast.LENGTH_SHORT).show();
-            }
-        });*/
+        btn_submit = (Button) findViewById(R.id.button_submit);
+        btn_add = (Button) findViewById(R.id.button_add);
+        editText=(EditText)findViewById(R.id.edit_name);
+//        btn_submit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "you clicked submit", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        btn_submit.setOnClickListener(this);
+        btn_add.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
         switch (v.getId()) {
             case R.id.button_submit:
                 Toast.makeText(this, "you clicked submit", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button_add:
-                Toast.makeText(this, "you clicked add", Toast.LENGTH_SHORT).show();
+                String inputText=editText.getText().toString();
+                Toast.makeText(this, inputText, Toast.LENGTH_SHORT).show();
+                /*Log.d("omen","YYYYY");*/
                 break;
             default:
                 break;
